ae9dd53aee37af2d32fb653ed902f78e20c28628
Alex Connolly <alex.connolly.webmaster@gmail.com>
2026-04-21 13:55:55 +0100

    fix: grain blend mode for white sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 export default function App() {
   return (
     <div className="overflow-x-hidden">
-      <svg className="fixed inset-0 w-full h-full z-[9999] pointer-events-none opacity-[0.15]" xmlns="http://www.w3.org/2000/svg">
+      <svg className="fixed inset-0 w-full h-full z-[9999] pointer-events-none" style={{ mixBlendMode: 'multiply', opacity: 0.25 }} xmlns="http://www.w3.org/2000/svg">
         <filter id="grain">
           <feTurbulence type="fractalNoise" baseFrequency="0.65" numOctaves="3" stitchTiles="stitch" />
         </filter>
