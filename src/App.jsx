efb8c8707de75d1495f9110ac92833aa88b3030a
Alex Connolly <alex.connolly.webmaster@gmail.com>
2026-04-21 13:54:43 +0100

    fix: inline SVG grain that actually renders

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ import Footer from './components/Footer'
 export default function App() {
   return (
     <div className="overflow-x-hidden">
-      <div className="fixed inset-0 z-[9999] pointer-events-none" style={{ backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='300' height='300'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.75' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='300' height='300' filter='url(%23n)' opacity='1'/%3E%3C/svg%3E")`, opacity: 0.12 }} />
+      <svg className="fixed inset-0 w-full h-full z-[9999] pointer-events-none opacity-[0.15]" xmlns="http://www.w3.org/2000/svg">
+        <filter id="grain">
+          <feTurbulence type="fractalNoise" baseFrequency="0.65" numOctaves="3" stitchTiles="stitch" />
+        </filter>
+        <rect width="100%" height="100%" filter="url(#grain)" />
+      </svg>
       <Nav />
       <Hero />
       {/* <Work /> */}
